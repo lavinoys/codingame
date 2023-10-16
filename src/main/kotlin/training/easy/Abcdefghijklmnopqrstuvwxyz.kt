b588aedc6c4955948a78ec6136196741cd9496e5
jay.moon <jay.moon@kakaocorp.com>
2023-10-16 15:38:05 +0900

    Abcdefghijklmnopqrstuvwxyz.kt best solution

--- src/main/kotlin/training/easy/Abcdefghijklmnopqrstuvwxyz.kt
+++ src/main/kotlin/training/easy/Abcdefghijklmnopqrstuvwxyz.kt
@@ -1,64 +1,44 @@
 //package training.easy
 
-
 fun main() {
-    val n = readLine()!!.toInt()
-    val multiLineString = mutableListOf<String>()
+    val input = Array(readln().toInt()) { readln().toCharArray().toTypedArray() }
+//        .onEach {
+//            System.err.println(it.joinToString(""))
+//        }
 
-    repeat(n) {
-        multiLineString.add(readLine()!!)
-    }
-
-    val visited = Array(n) { BooleanArray(n) }
-    val alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-    fun isValid(x: Int, y: Int) = x in 0 until n && y in 0 until n
-
-    fun dfs(x: Int, y: Int, charIndex: Int): Boolean {
-        if (charIndex == alphabet.length) {
-            return true
-        }
-
-        if (!isValid(x, y) || visited[x][y] || multiLineString[x][y] != alphabet[charIndex]) {
-            return false
-        }
-
-        visited[x][y] = true
-
-        val dx = intArrayOf(1, -1, 0, 0)
-        val dy = intArrayOf(0, 0, 1, -1)
-
-        for (i in 0 until 4) {
-            val newX = x + dx[i]
-            val newY = y + dy[i]
-
-            if (dfs(newX, newY, charIndex + 1)) {
-                return true
-            }
-        }
-
-        visited[x][y] = false
-        return false
-    }
-
-    for (i in 0 until n) {
-        for (j in 0 until n) {
-            if (multiLineString[i][j] == 'a' && dfs(i, j, 0)) {
-                for (x in 0 until n) {
-                    for (y in 0 until n) {
-                        if (!visited[x][y]) {
-                            multiLineString[x] =
-                                multiLineString[x].substring(0, y) + '-' + multiLineString[x].substring(y + 1)
-                        }
-                    }
-                }
-            }
+    val starts = input.flatMapIndexed { y, line ->
+        line.mapIndexedNotNull { x, c ->
+            if (c == 'a') y to x else null
         }
     }
+//        .onEach {
+//            System.err.println("${it.first} ${it.second}")
+//        }
 
-    for (line in multiLineString) {
-        println(line)
+    for ((y, x) in starts) {
+        val (success, chain) = input.dfs('a', y, x)
+//        System.err.println("$success ${chain.joinToString(",")}")
+        if (success) return input.solve(chain)
     }
 }
 
+fun Array<Array<Char>>.dfs(c: Char, y: Int, x: Int): Pair<Boolean, List<Pair<Int, Int>>> {
+    if (c == 'z') return true to listOf(y to x)
+    listOf(0 to 1, 0 to -1, 1 to 0, -1 to 0).forEach { (dy, dx) ->
+        if (y + dy in indices && x + dx in indices && this[y + dy][x + dx] == c + 1) {
+            val res = dfs(c + 1, y + dy, x + dx)
+            if (res.first) return@dfs true to listOf(y to x) + res.second
+        }
+    }
+    this[y][x] = '-'
+    return false to listOf()
+}
 
+fun Array<Array<Char>>.solve(chain: List<Pair<Int, Int>>) {
+    indices.forEach { a ->
+        indices.forEach { b ->
+            if (a to b !in chain) this[a][b] = '-'
+        }
+    }
+    println(joinToString("\n") { it.joinToString("") })
+}
