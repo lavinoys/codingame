3ff607ceb7d8eafb52e6634e503cadd8acaa298d
jay <devjay0530@gmail.com>
2025-05-04 01:19:30 +0900

    Add Travelling Salesman problem implementation and documentation

---
+++ src/main/kotlin/solo/optimization/travellingsalesman/TravellingSalesman.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fun main(args : Array<String>) {
+    val input = Scanner(System.`in`)
+    val N = input.nextInt() // This variables stores how many nodes are given
+    for (i in 0 until N) {
+        val X = input.nextInt() // The x coordinate of the given node
+        val Y = input.nextInt() // The y coordinate of the given node
+    }
+
+    // Write an action using println()
+    // To debug: System.err.println("Debug messages...");
+
+    println("0 2 1 3") // You have to output a valid path
+}
