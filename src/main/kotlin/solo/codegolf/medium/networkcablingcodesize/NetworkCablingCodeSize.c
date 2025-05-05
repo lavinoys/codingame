48609015e9af0620ad68e6b366ee2a6f226449d4
jay <devjay0530@gmail.com>
2025-05-06 01:14:03 +0900

    Add Network Cabling Code Size problem; implement input parsing in C

---
+++ src/main/kotlin/solo/codegolf/medium/networkcablingcodesize/NetworkCablingCodeSize.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) {
+        int X;
+        int Y;
+        scanf("%d%d", &X, &Y);
+    }
+
+    // Write an answer using printf(). DON'T FORGET THE TRAILING \n
+    // To debug: fprintf(stderr, "Debug messages...\n");
+
+    printf("answer\n");
+
+    return 0;
+}
