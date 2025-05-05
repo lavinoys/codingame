cbefa98bbde61c509d4460b703f65a1379cb134f
jay <devjay0530@gmail.com>
2025-05-05 22:50:03 +0900

    Add main function to WordleJni; implement input handling for word set and game loop structure

--- src/main/kotlin/solo/optimization/wordle/WordleJni.kt
+++ src/main/kotlin/solo/optimization/wordle/WordleJni.kt
@@ -1,4 +1,26 @@
-package solo.optimization.wordle
+import java.util.*
+import java.io.*
+import java.math.*
 
-class WordleJni {
+/**
+ * Made by Tanvir
+ **/
+fun main(args : Array<String>) {
+    val input = Scanner(System.`in`)
+    val wordCount = input.nextInt() // Number of words in the word set
+    for (i in 0 until wordCount) {
+        val word = input.next() // Word in the word set
+    }
+
+    // game loop
+    while (true) {
+        for (i in 0 until 6) {
+            val state = input.nextInt() // State of the letter of the corresponding position of previous guess
+        }
+
+        // Write an action using println()
+        // To debug: System.err.println("Debug messages...");
+
+        println("ANSWER")
+    }
 }
