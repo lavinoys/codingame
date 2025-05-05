e7be75caa82d7c9c4be6bd15be959db8aa43e0f7
jay <devjay0530@gmail.com>
2025-05-06 00:44:16 +0900

    Add Bender episode 4; implement input parsing for maze game setup

---
+++ src/main/kotlin/solo/optimization/benderepisode4/BenderEpisode4.kt
@@ -0,0 +1,36 @@
+import java.util.*
+import java.io.*
+import java.math.*
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fun main(args : Array<String>) {
+    val input = Scanner(System.`in`)
+    val width = input.nextInt()
+    val height = input.nextInt()
+    if (input.hasNextLine()) {
+        input.nextLine()
+    }
+    for (i in 0 until height) {
+        val line = input.nextLine()
+    }
+    val startX = input.nextInt()
+    val startY = input.nextInt()
+    val targetX = input.nextInt()
+    val targetY = input.nextInt()
+    val switchCount = input.nextInt()
+    for (i in 0 until switchCount) {
+        val switchX = input.nextInt()
+        val switchY = input.nextInt()
+        val blockX = input.nextInt()
+        val blockY = input.nextInt()
+        val initialState = input.nextInt() // 1 if blocking, 0 otherwise
+    }
+
+    // Write an action using println()
+    // To debug: System.err.println("Debug messages...");
+
+    println("RRRRDDDDRRRRUUUULRUUUUUUUUUUUUUDDUDDDDDDDDDDDDDDDDLLLLUUUUUUUULLUULLRRDDRRDDDDDDDDLLLLLLUULLUUUULLUUUUUUL")
+}
