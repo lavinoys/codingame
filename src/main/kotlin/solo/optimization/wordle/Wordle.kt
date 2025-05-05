b5d7b670cb71cf64f2e12bf06a5b87130f2c2674
jay <devjay0530@gmail.com>
2025-05-05 22:23:59 +0900

    Add initial Wordle game implementation; set up input handling and game loop structure

---
+++ src/main/kotlin/solo/optimization/wordle/Wordle.kt
@@ -0,0 +1,26 @@
+import java.util.*
+import java.io.*
+import java.math.*
+
+/**
+ * Made by Tanvir
+ **/
+fun main(args : Array<String>) {
+    val input = Scanner(System.`in`)
+    val wordCount = input.nextInt() // Number of words in the word set
+    for (i in 0 until wordCount) {
+        val word = input.next() // Word in the word set
+    }
+
+    // game loop
+    while (true) {
+        for (i in 0 until 6) {
+            val state = input.nextInt() // State of the letter of the corresponding position of previous guess
+        }
+
+        // Write an action using println()
+        // To debug: System.err.println("Debug messages...");
+
+        println("ANSWER")
+    }
+}
