2f126f81444ae05ebfea8baa68cd82b426d704a8
jay <devjay0530@gmail.com>
2025-05-06 02:00:56 +0900

    Add Reverse Mode Case 02; implement solution in Kotlin and Python

--- src/main/kotlin/multi/clash/Reversemode/case02/ReverseModeCase02.kt
+++ src/main/kotlin/multi/clash/Reversemode/case02/ReverseModeCase02.kt
@@ -1,17 +1,9 @@
 import java.util.*
-import java.io.*
-import java.math.*
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 fun main(args : Array<String>) {
     val input = Scanner(System.`in`)
     val n = input.nextInt()
 
-    // Write an answer using println()
-    // To debug: System.err.println("Debug messages...");
-
-    println("answer")
+    val result = (n * 3) / 2
+    println(result)
 }
---
+++ src/main/kotlin/multi/clash/Reversemode/case02/ReverseModeCase02.py
@@ -0,0 +1,8 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+print(n + n // 2)
