0f0a57a1db9a6cb5d6b2fe2f0ec90667637be3b4
jay <devjay0530@gmail.com>
2025-05-06 02:07:20 +0900

    Add Shortest Mode Case 01; implement problem description and solution template in C and markdown

---
+++ src/main/kotlin/multi/clash/shortestmode/case01/ShortestModeCase01.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+/**
+ * Find the sum of the first n odd integers!
+ * (e.g. n=4: 1+3+5+7=16)
+ **/
+
+int main()
+{
+    // the number of odd integers
+    int n;
+    scanf("%d", &n);
+
+    // Write an answer using printf(). DON'T FORGET THE TRAILING \n
+    // To debug: fprintf(stderr, "Debug messages...\n");
+
+    printf("answer\n");
+
+    return 0;
+}
