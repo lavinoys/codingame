50334a150e98d86c8464c6747dd476098458737d
jay <devjay0530@gmail.com>
2025-05-06 12:03:37 +0900

    Add FastestModCase01; implement input parsing and output for sum of even and odd numbers

---
+++ src/main/kotlin/multi/clash/fastestmod/case01/FastestModCase01.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) {
+        int M;
+        scanf("%d", &M);
+    }
+
+    // Write an answer using printf(). DON'T FORGET THE TRAILING \n
+    // To debug: fprintf(stderr, "Debug messages...\n");
+
+    printf("answer\n");
+
+    return 0;
+}
---
+++ src/main/kotlin/multi/clash/fastestmod/case01/FastestModCase01.md
@@ -0,0 +1,39 @@
+# FastestModCase01
+
+## Goal
+
+Your task is to write a program that takes a sequence M of integers of length N and returns two numbers:
+
+The sum of even numbers.
+The sum of odd numbers.
+
+## Input
+
+Line 1: An integer N for the length of M
+Next N lines: Numbers to add.
+
+## Output
+
+Sum of even numbers
+
+and
+sum of odd numbers
+
+separated by space.
+
+## Constraints
+
+3 <= N <= 200
+
+## Example
+
+### Input
+
+3
+1
+2
+3
+
+### Output
+
+2 4
